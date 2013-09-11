ccc91d557eaec6aa27b2cc1751b638d9b9eece6d
Jesse Weaver <pianohacker@gmail.com>
2013-09-10 18:54:59 -0600

    Initial scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScheduleMiner::Application.config.secret_token = 'ce4d29f3e4dd03acdc7399de69f437c968bee33e7d17e6256701caab947af287655ccc4568b6efd8d9cc2fd7a9e34c731d94ba3c912265d036064b5cf64ff316'
