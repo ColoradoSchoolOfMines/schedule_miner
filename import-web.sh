8c6927ff1dd26548c54e4e92ba2ceb4e70ecfbd7
Jesse Weaver <pianohacker@gmail.com>
2013-10-29 18:42:35 -0600

    Update filename and term for download script

--- import-web.sh
+++ import-web.sh
@@ -15,4 +15,4 @@
 ######
 ######
 ######
-curl -d 'term_in=201380&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj=&sel_crse=&sel_title=&sel_schd=%25&sel_from_cred=&sel_to_cred=&sel_levl=%25&sel_ptrm=%25&sel_instr=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a' https://banner.mines.edu/prod/owa/bwckschd.p_get_crse_unsec > /tmp/MinerImport
+curl -d 'term_in=201410&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj=&sel_crse=&sel_title=&sel_schd=%25&sel_from_cred=&sel_to_cred=&sel_levl=%25&sel_ptrm=%25&sel_instr=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a' https://banner.mines.edu/prod/owa/bwckschd.p_get_crse_unsec > /tmp/courses
