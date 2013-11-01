63c0ce00039625030f31d3de8d136379764d2c92
Tyler Thrailkill <tylerbthrailkill@gmail.com>
2013-10-31 23:21:11 -0600

    Update look, remove index.html, added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ScheduleMiner::Application.routes.draw do
+  get "static/index"
+
   resources :sections
 
   resources :departments
@@ -59,6 +61,7 @@ ScheduleMiner::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root to: 'static#index', as: 'static'
 
   # See how all your routes lay out with "rake routes"
 
