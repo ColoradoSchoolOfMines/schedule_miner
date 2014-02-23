09f645c5fd798b67aca80213077560ecd4b48084
Jesse Weaver <pianohacker@gmail.com>
2014-02-22 17:45:15 -0700

    Add migration to fix column type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140222233327) do
+ActiveRecord::Schema.define(:version => 20140223001036) do
 
   create_table "buildings", :force => true do |t|
     t.string   "code"
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(:version => 20140222233327) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.decimal  "user_id"
+    t.integer  "user_id"
   end
 
   create_table "sections", :force => true do |t|
