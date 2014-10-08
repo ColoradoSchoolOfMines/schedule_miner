69960be2ab7f476cd608e5e35875da330c203de7
Steve <shamrick@mymail.mines.edu>
2014-10-07 18:53:33 -0700

    Progress on schedule builder

--- app/assets/javascripts/builder.js
+++ app/assets/javascripts/builder.js
@@ -7,12 +7,45 @@ jQuery.ajax({
 	}
 });
 
+function getSections(id){
+	jQuery.ajax({
+		url: '/courses/' + id + '/sections.json',
+		type: 'GET',
+		dataType: 'script',
+		success: function(response){
+			return response;
+		}
+	});
+}
 
 function courseAuto(response){
 	var obj = jQuery.parseJSON(response);
+	var departments = [];
+	for(var i = 0; i < obj.length; i++){
+		if(departments.indexOf(obj[i].department_code) <= -1) departments.push(obj[i].department_code);
+	}
+	jQuery('#Department').typeahead({
+		hint: true,
+		highlight: true,
+		minLength: 1
+	},
+	{
+		name: 'departments',
+		displayKey: 'value',
+		source: substringMatcher(departments)
+	});
 
 	jQuery('#Department').blur(function(){
-		if(objContains(jQuery(this).val(), obj) > -1) console.log('ok');
+		var index = objContains(jQuery(this).val(), obj);
+		if(index > -1){
+			var sections = [];
+			for(var i = 0; i < obj.length; i++){
+				if(obj[i].department_code == jQuery('#Department').val() && sections.indexOf(obj[i].department_code) <= -1){
+					sections.push(obj[i].id);
+				}
+			}
+			console.log(sections);
+		}
 		else jQuery(this).val('');
 	});
 }
@@ -24,3 +57,21 @@ function objContains(value, objects){
 	}
 	return index;
 }
+
+var substringMatcher = function(strs) {
+	return function findMatches(q, cb) {
+		var matches, substrRegex;
+		 
+		matches = [];
+		 
+		substrRegex = new RegExp(q, 'i');
+		 
+		jQuery.each(strs, function(i, str) {
+			if (substrRegex.test(str)) {
+				matches.push({ value: str });
+			}
+		});
+		 
+		cb(matches);
+	};
+};
